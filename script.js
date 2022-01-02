ff8d7d2a32699bc91797e68e0f988b5aaa1dff07
MITHIRI1 <87658049+MITHIRI1@users.noreply.github.com>
2022-01-02 11:40:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //doubleValues
 //Write a function called doubleValues which accepts an array and returns a new array with all the values in the array passed to the function doubled
 
-//doubleValuehhkbkbkj kjkjbkj4 ,6]
+//doubleValues([1,2,3]) // [2,4,6]
 //doubleValues([5,1,2,3,10]) // [10,2,4,6,20]
 
 function doubleValues(arr) {
